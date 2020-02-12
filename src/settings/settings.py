662290f2417a275fb48442946facff42df5d5ec2
Håvard O. Nordstrand <haavard@haavard.tech>
2020-02-12 07:20:26 +0100

    Fix static files

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -83,7 +83,36 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' 'static/' subdirectories and in STATICFILES_DIRS.
+# Example: '/home/media/media.lawrence.com/static/'
+STATIC_ROOT = 'static/'
+
+# URL prefix for static files.
+# Example: 'http://media.lawrence.com/static/'
 STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: 'http://foo.com/static/admin/', '/static/admin/'.
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'res'),
+    # Put strings here, like '/home/html/static' or 'C:/www/django/static'.
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+]
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # Read version from file
 VERSION = 'Unknown version'
